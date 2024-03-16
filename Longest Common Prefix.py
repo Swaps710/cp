0c0d937e37bf20ca6d702c2521d654e26f859124
Swaps710 <147497871+Swaps710@users.noreply.github.com>
2024-03-16 10:58:20 +0530

    Longest Common Prefix.py

---
+++ Longest Common Prefix.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        l = list(zip(*strs))
+        print(l)
+        prefix=""
+        for i in l:
+            if len(set(i))==1:
+                prefix +=i[0]
+            else:
+                break
+        return prefix
+        
